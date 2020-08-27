33255e9105698ff32058c44bf04d9a653a062a3b
Lucas Seara Manoel <ls.manoel@gmail.com>
2020-08-27 18:19:12 -0300

    refactor: move header and foorwe to separate files

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
-import Logo from '@/presentation/components/logo/logo'
+import Header from '@/presentation/components/login-header/login-header'
+import Footer from '@/presentation/components/footer/footer'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>dev - Enquetes para Programadores</h1>
-      </header>
+      <Header />
       <form className={Styles.form}>
         <h2>Login</h2>
         <div className={Styles.inputWrap}>
@@ -23,13 +21,11 @@ const Login: React.FC = () => {
         <button className={Styles.submit} type="submit">Entrar</button>
         <span className={Styles.link}>Criar uma conta</span>
         <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner}/>
+          <Spinner className={Styles.spinner} />
           <span className={Styles.error}>Erro</span>
         </div>
       </form>
-      <footer className={Styles.footer}>
-
-      </footer>
+      <Footer />
     </div>
   )
 }
