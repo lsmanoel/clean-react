15a8aff899724b4af2db83557feb039bfb23c961
Lucas Seara Manoel <ls.manoel@gmail.com>
2020-08-30 13:02:42 -0300

    feat: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/required-field/email/email-validation.ts
+++ src/validation/validators/required-field/email/email-validation.ts
@@ -3,8 +3,8 @@ import { InvalidFieldError } from '@/validation/errors'
 
 export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
-
   validate (value: string): Error {
-    return new InvalidFieldError()
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return emailRegex.test(value) ? null : new InvalidFieldError()
   }
 }
