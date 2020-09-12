6e64cfca7c296102414703d0d03c14514d22855e
Lucas Seara Manoel <ls.manoel@gmail.com>
2020-09-12 14:50:56 -0300

    test: ensure SingUp hode spinner and present error if AddAccount fails

--- src/presentation/pages/singup/singup.spec.tsx
+++ src/presentation/pages/singup/singup.spec.tsx
@@ -3,6 +3,7 @@ import faker from 'faker'
 import { render, RenderResult, cleanup, fireEvent, waitFor } from '@testing-library/react'
 import SingUp from './singup'
 import { Helper, ValidationStub, AddAccountSpy } from '@/presentation/test'
+import { EmailInUseError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RenderResult
@@ -37,6 +38,11 @@ const simulateValidSubmit = async (sut: RenderResult, name = faker.random.word()
   const form = sut.getByTestId('form')
   fireEvent.submit(form)
   await waitFor(() => form)
+}
+
+const testElementText = (sut: RenderResult, fieldName: string, text: string): void => {
+  const el = sut.getByTestId(fieldName)
+  expect(el.textContent).toBe(text)
 }
 
 describe('SingUp Component', () => {
@@ -141,5 +147,14 @@ describe('SingUp Component', () => {
     const { sut, addAccountSpy } = makeSut({ validationError })
     await simulateValidSubmit(sut)
     expect(addAccountSpy.callsCount).toBe(0)
+  })
+
+  test('Should present error if AddAccount fails', async () => {
+    const { sut, addAccountSpy } = makeSut()
+    const error = new EmailInUseError()
+    jest.spyOn(addAccountSpy, 'add').mockRejectedValueOnce(error)
+    await simulateValidSubmit(sut)
+    testElementText(sut, 'message-error', error.message)
+    Helper.testChildCount(sut, 'error-wrap', 1)
   })
 })
