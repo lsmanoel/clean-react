262cca5d70291b16bdfb9d942a31d4e6713dd907
Lucas Seara Manoel <ls.manoel@gmail.com>
2020-09-12 21:57:03 -0300

    test: add SingUpValidationFactory

---
+++ src/main/factories/pages/singup/singup-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { makeSingUpValidation } from './singup-validation-factory'
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+describe('SingUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSingUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
